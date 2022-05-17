f78d1a14cd2a8284282664e1347f47faae16f2e9
kayacancode <62274079+kayacancode@users.noreply.github.com>
2022-05-17 11:52:58 -0500

    style improvements

--- components/ContactCard.js
+++ components/ContactCard.js
@@ -9,19 +9,18 @@ const ContactCard = (info) => {
     <div class = "bg-[#F5F5F5] ">
         <div class = "text-center justify-items-center  bg-white ">
         <div class = "flex grid justify-items-start ">
-                    <div class ="grid grid-cols-2 pt-5 ">
+                    <div class ="grid grid-cols-2 pt-5 pl-3">
                         <Link href ="/Closefreinds">
                     <img src= "/arrow.png" class = "realitve float-left cursor-pointer "width="100%" height="100%" layout="responsive" objectFit="contain" />
                     
                     </Link>
-                    <Link href= "/Closefreinds">
+                    {/* <Link href= "/Closefreinds">
                     <a href = "" class= "relative text-[#007aff] ">Back</a>
-                    </Link>
+                    </Link> */}
                </div>
                </div>
-               <div class = "pt-5 pb-24">
-                <img src= "/contactpic.png" width="85px" height="85px"  />
-                <h1 class = "font-bold  text-2xl">{info.name}</h1>
+               <div class = "pt-5 pb-24  ">
+                <h1 class = "font-bold  text-5xl">{info.name}</h1>
                 </div>
     </div>
 
@@ -30,19 +29,16 @@ const ContactCard = (info) => {
 
     <div class = "">
                         <div class = "pb-4 ">
-                            <h1>Website: <a href={info.websitelink}>{info.websitename}</a>
-</h1>
+                  <h1 class = "py-2 font-bold">Instagram: <a href={info.instaLink}>{info.insta}</a></h1>
+
 
                         </div>
                         
                         <div class = "pb-4">
 
-                        <h1 class = "py-2">Notes:</h1>
                         <h1 class = "py-2">Email:<a href={info.emailLink}>{info.email}</a></h1>
-                        <h1 class = "py-2">Instagram: <a href={info.instaLink}>{info.insta}</a></h1>
-                        <h1 class = "py-2">Twitter:<a href={info.twitterLink}>{info.twitter}</a></h1>
-                        <h1 class = "py-2">Tiktok:<a href={info.tiktokLink}>{info.tiktok}</a></h1>
-                        <h1 class = "py-2">Youtube:<a href={info.ytlink}>{info.yt}</a></h1>
+                        <h1>Website: <a href={info.websitelink}>{info.websitename}</a></h1>   
+                        <h1 class = "py-2 ">Twitter:<a href={info.twitterLink}>{info.twitter}</a></h1>
 
                         </div>
                        
