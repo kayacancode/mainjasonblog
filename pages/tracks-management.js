be379b7b03a5bd3eb27519e4da741e5ab8beb6aa
ShayanMB <112359137+ShayanMB@users.noreply.github.com>
2025-09-06 17:00:25 -0500

    Added disclaimer about popularity score of 0 to track management page

--- pages/tracks-management.js
+++ pages/tracks-management.js
@@ -643,6 +643,25 @@ export default function TracksManagement() {
                         </div>
                     </div>
                 </div>
+
+                {/* Popularity Note */}
+                <div className="mt-6 bg-amber-50 border border-amber-200 rounded-lg p-4">
+                    <div className="flex items-start">
+                        <div className="flex-shrink-0">
+                            <svg className="w-5 h-5 text-amber-400 mt-0.5" fill="currentColor" viewBox="0 0 20 20">
+                                <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clipRule="evenodd" />
+                            </svg>
+                        </div>
+                        <div className="ml-3">
+                            <h3 className="text-sm font-medium text-amber-800">About Popularity Scores</h3>
+                            <div className="mt-1 text-sm text-amber-700">
+                                <p>Some tracks may show a popularity score of 0. This is normal for some newly released tracks, as the weekly music update pulls at a scheduled time,
+                                    and Spotify may not have assigned a popularity score yet.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
 
             {/* Add/Edit Form */}
